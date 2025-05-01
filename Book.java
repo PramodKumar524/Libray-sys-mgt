b3d21b7fb680435a77d3f085df387d0909457f28
PRAMOD KUMAR R <pramodkumarraj129@gmail.com>
2025-05-01 12:22:17 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,41 @@
+package LiBRARY_MANAGEMENT;
+
+class Book {
+    private String title;
+    private String author;
+    private boolean isIssued;
+    private Student issuedTo; 
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isIssued = false;
+        this.issuedTo = null;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isIssued() {
+        return isIssued;
+    }
+
+    public void issue(Student student) {
+        isIssued = true;
+        issuedTo = student;
+    }
+
+    public void returnBook() {
+        isIssued = false;
+        issuedTo = null;
+    }
+
+    @Override
+    public String toString() {
+        if (isIssued) {
+            return title + " by " + author + " (Issued) - " + issuedTo.getDetails();
+        }
+        return title + " by " + author + " (Available)";
+    }
+}
